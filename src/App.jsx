75d3a4ef56157c0b93f17f02c44aa3dc4816d7b6
USER-28 <USER-28@growmore.local>
2026-09-11 08:49:36 +0530

    Fix App routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,44 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import './App.css'
-import Footer from './Components/Footer.jsx'
-import Navbar from './Components/Navbar.jsx'
-import Home from './Pages/Home'
-import AboutUs from './Pages/AboutUs.jsx'
-import Work from './Pages/Work.jsx'
-import Migration from './Pages/Migration.jsx'
-import Dependent from './Pages/Dependent.jsx'
-import Visitor from './Pages/Visitor.jsx'
-import Student from './Pages/Student.jsx'
-import Videos from './Pages/Videos.jsx'
-import English from './Pages/English.jsx'
-import Ielts from './Pages/Ielts'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+import Navbar from "./Components/Navbar.jsx";
+import Footer from "./Components/Footer.jsx";
+
+import Home from "./Pages/Home.jsx";
+import AboutUs from "./Pages/AboutUs.jsx";
+import Work from "./Pages/Work.jsx";
+import Migration from "./Pages/Migration.jsx";
+import Dependent from "./Pages/Dependent.jsx";
+import Visitor from "./Pages/Visitor.jsx";
+import Student from "./Pages/Student.jsx";
+import Videos from "./Pages/Videos.jsx";
+import English from "./Pages/English.jsx";
+import Ielts from "./Pages/Ielts.jsx";
 
 function App() {
-
   return (
-    <>
+    <Router>
       <Navbar />
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/who-we-are' element={<AboutUs />} />
-          <Route path='/services/work-visa' element={<Work />} />
-          <Route path='/services/migration-visa' element={<Migration />} />
-          <Route path='/services/dependent-visa' element={<Dependent />} />
-          <Route path='/services/visitor-visa' element={<Visitor />} />
-          <Route path='/services/student-visa' element={<Student />} />
-          <Route path='/latest-update/videos' element={<Videos />} />
-          <Route path='/english-test' element={<English />} />
-          <Route path='/english-test/ielts' element={<Ielts />} />
-        </Routes>
-      </Router>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/who-we-are" element={<AboutUs />} />
+
+        <Route path="/services/work-visa" element={<Work />} />
+        <Route path="/services/migration-visa" element={<Migration />} />
+        <Route path="/services/dependent-visa" element={<Dependent />} />
+        <Route path="/services/visitor-visa" element={<Visitor />} />
+        <Route path="/services/student-visa" element={<Student />} />
+
+        <Route path="/latest-update/videos" element={<Videos />} />
+
+        <Route path="/english-test" element={<English />} />
+        <Route path="/english-test/ielts" element={<Ielts />} />
+      </Routes>
+
       <Footer />
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
