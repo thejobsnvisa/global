e6a2cba459564fa7a16b3a736140e499957049e4
USER-28 <USER-28@growmore.local>
2026-09-11 08:44:01 +0530

    code added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './Components/Footer.jsx'
 import Navbar from './Components/Navbar.jsx'
@@ -10,29 +10,29 @@ import Dependent from './Pages/Dependent.jsx'
 import Visitor from './Pages/Visitor.jsx'
 import Student from './Pages/Student.jsx'
 import Videos from './Pages/Videos.jsx'
-import English from "./Pages/English.jsx"
-import Ielts from './Pages/Ielts.jsx'
+import English from './Pages/English.jsx'
+import Ielts from './Pages/Ielts'
 
 function App() {
 
   return (
     <>
-    <Navbar />
-    <Router>
-      <Routes>
-        <Route path='/' element = {<Home/>} />
-        <Route path='/who-we-are' element={<AboutUs/>}/>
-        <Route path='/services/work-visa' element={<Work/>}/>
-        <Route path='/services/migration-visa' element={<Migration/>}/>
-        <Route path='/services/dependent-visa' element={<Dependent/>}/>
-        <Route path='/services/visitor-visa' element={<Visitor/>}/>
-        <Route path='/services/student-visa' element={<Student/>}/>
-        <Route path='/latest-update/videos'  element={<Videos/>}/>
-        <Route path='/english-test' element={<English/>}/>
-        <Route path='/english-test/ielts' element={<Ielts/>}/>
-      </Routes>
-    </Router>
-    <Footer />
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/who-we-are' element={<AboutUs />} />
+          <Route path='/services/work-visa' element={<Work />} />
+          <Route path='/services/migration-visa' element={<Migration />} />
+          <Route path='/services/dependent-visa' element={<Dependent />} />
+          <Route path='/services/visitor-visa' element={<Visitor />} />
+          <Route path='/services/student-visa' element={<Student />} />
+          <Route path='/latest-update/videos' element={<Videos />} />
+          <Route path='/english-test' element={<English />} />
+          <Route path='/english-test/ielts' element={<Ielts />} />
+        </Routes>
+      </Router>
+      <Footer />
     </>
   )
 }
